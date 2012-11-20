31c108553fbb8df4a919061191ac8736bd36cc93
ZNATZ <Z.NAT.CN@GMAIL.COM>
2012-11-20 17:27:35 +0900

    Demonstration of variablesizegrid

---
+++ ColorWrap/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using NATControl;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+namespace ColorWrap
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            IEnumerable<PropertyInfo> propertyinfos = typeof(Colors).GetTypeInfo().DeclaredProperties;
+            foreach (PropertyInfo pInfo in propertyinfos)
+            {
+                ColorItemCtrl ctrl = new ColorItemCtrl(pInfo.Name, (Color)pInfo.GetValue(null));
+                wrapGrid.Children.Add(ctrl);
+            }
+        }
+    }
+}
